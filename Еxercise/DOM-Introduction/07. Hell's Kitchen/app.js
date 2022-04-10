8fb9e3a75aacbd0dc991f691a93781b3f1dfbc46
kStoykow <kristiqn.stoikow11@gmail.com>
2022-04-10 23:06:31 +0300

    Adding Hells kitchen solution.

--- Еxercise/DOM-Introduction/07. Hell's Kitchen/app.js
+++ Еxercise/DOM-Introduction/07. Hell's Kitchen/app.js
@@ -3,14 +3,70 @@ function solve() {
 
    function clickHandler() {
       let btn = document.getElementById('btnSend');
-      let input = document.querySelector('#inputs textarea');
-      let bestRestourantP = document.querySelector('#bestRestaurant p');
-      let workersP = document.querySelector('#workers p');
-
-      if (btn == null || input == null || bestRestourantP == null || workersP == null) {
+      let inputElem = document.querySelector('#inputs textarea');
+      let bestRestourantPElem = document.querySelector('#bestRestaurant p');
+      let workersPElem = document.querySelector('#workers p');
+      if (btn == null || inputElem == null || bestRestourantPElem == null || workersPElem == null) {
          throw new Error('Missing DOM element!');
       }
-   
-      
+
+      let data = JSON.parse(inputElem.value);
+      function restourantsFactory(data) {
+         let restourants = data.reduce((a, b) => {
+            let [restourant, workersInfo] = b.split(' - ');
+            let ppl = workersInfo.split(', ');
+
+            let currRestourant = a.find(e => e.name == restourant);
+
+            if (currRestourant == undefined) {
+               let currWorkers = ppl.reduce((a, b) => {
+                  let [name, salary] = b.split(' ');
+                  a[name] = Number(salary);
+                  return a;
+               }, {});
+               a.push({
+                  name: restourant,
+                  workers: currWorkers,
+               })
+
+            } else {
+               ppl.forEach((e) => {
+                  let [name, salary] = e.split(' ');
+                  currRestourant.workers[name] = Number(salary);
+               });
+            }
+
+            return a;
+         }, []);
+         return restourants;
+      }
+      function bestRestourantInfo(restourants) {
+         let bestRestourant = '';
+         let avgSalary = 0;
+         let bestSalary = 0;
+
+         for (const e of restourants) {
+            let currAvg = Object.values(e.workers).reduce((a, b) => a + b, 0) / Object.values(e.workers).length;
+
+            if (currAvg > avgSalary) {
+               bestRestourant = e.name;
+               avgSalary = currAvg;
+               bestSalary = Math.max(...Object.values(e.workers));
+            }
+         }
+         let best = {
+            restourant: bestRestourant,
+            avg: avgSalary,
+            salary: bestSalary
+         }
+         return best;
+      }
+
+      const restourants = restourantsFactory(data);
+      const best = bestRestourantInfo(restourants);
+
+      bestRestourantPElem.textContent = `Name: ${best.restourant} Average Salary: ${best.avg.toFixed(2)} Best Salary: ${best.salary.toFixed(2)}`;
+      let workersOutput = Object.entries(restourants.find(e => e.name == best.restourant).workers).sort((a, b) => b[1] - a[1]).map(([name, salary] = e) => `Name: ${name} With Salary: ${salary}`).join(' ');
+      workersPElem.textContent = `${workersOutput}`;
    }
 }
--- Еxercise/DOM-Introduction/08. Generate Report/report.js
+++ Еxercise/DOM-Introduction/08. Generate Report/report.js
@@ -1,3 +1,11 @@
 function generateReport() {
-    //TODO
+    let headersTr = document.querySelector('table>thead>tr:nth-child(1)');
+    let headersInputs = Array.from(headersTr.querySelectorAll('input'));
+    let dataTr = document.querySelectorAll('tbody tr');
+
+    let checkedFilterElem = headersInputs.filter(e => e.checked);
+    let checked = checkedFilterElem.map(e => e.name);
+    let firstRow = dataTr[0]
+    console.log(headersTr, checkedFilterElem);
+    console.log(firstRow);
 }
