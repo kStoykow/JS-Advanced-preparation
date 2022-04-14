9998c8dc27efce2213939c5dfb4382aa19dd8ffb
kStoykow <kristiqn.stoikow11@gmail.com>
2022-04-14 12:43:57 +0300

    Adding solutions.

--- Lab/Dom-manipulations/04. Mouse-Gradient/app.js
+++ Lab/Dom-manipulations/04. Mouse-Gradient/app.js
@@ -1,3 +1,14 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let boxElem = document.getElementById('gradient');
+    let resultElem = document.getElementById('result');
+
+    if (boxElem == null || resultElem == null) {
+        throw new Error('Missing DOM element');
+    }
+    
+    function mousemoveHandler(e) {
+        resultElem.textContent = `${Math.floor((Number(e.offsetX) / Number(e.target.clientWidth)) * 100)}%`;
+    }
+
+    boxElem.addEventListener('mousemove', mousemoveHandler);
 }
--- Lab/Dom-manipulations/05. Highlight-Active/app.js
+++ Lab/Dom-manipulations/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+    let divElem = document.querySelector('div');
+
+    if (divElem == null) {
+        throw new Error('Missing DOM element');
+    }
+
+    function focusHandler(e) {
+        if (e.target != null && e.target.nodeName == 'INPUT') {
+            e.target.parentNode.className = 'focused';
+        }
+    }
+
+    function blurHandler(e) {
+        e.target.parentNode.className = '';
+    }
+
+    divElem.addEventListener('focus', focusHandler, true);
+    divElem.addEventListener('blur', blurHandler, true);
 }
